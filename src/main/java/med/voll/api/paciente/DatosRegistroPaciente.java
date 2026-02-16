994af5be2597e4c4c20405543a8e4d15b0523c5b
Miguel Ángel de la Cruz Lázaro <256243310+Miguel-Dark@users.noreply.github.com>
2026-02-16 01:46:54 -0600

    feat(api): implementar PacienteController y DTO DatosRegistroPaciente con reutilización de Direccion

---
+++ src/main/java/med/voll/api/paciente/DatosRegistroPaciente.java
@@ -0,0 +1,6 @@
+package med.voll.api.paciente;
+
+import med.voll.api.direccion.Direccion;
+
+public record DatosRegistroPaciente(String nombre, String email, String telefono, String documento, Direccion direccion) {
+}
