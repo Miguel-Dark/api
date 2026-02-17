b1a7e54c3ae2c7bade5848fc6bcbd4ac0cc1502b
Miguel Ángel de la Cruz Lázaro <256243310+Miguel-Dark@users.noreply.github.com>
2026-02-17 03:26:50 -0600

    feat: mapeo de entidades Medico y Direccion con anotaciones JPA y Lombok

--- src/main/java/med/voll/api/paciente/DatosRegistroPaciente.java
+++ src/main/java/med/voll/api/paciente/DatosRegistroPaciente.java
@@ -1,11 +1,11 @@
 package med.voll.api.paciente;
 
-import med.voll.api.direccion.Direccion;
+import med.voll.api.direccion.DatosDireccion;
 //DTO
 public record DatosRegistroPaciente(
         String nombre,
         String email,
         String telefono,
         String documentoIdentidad,
-        Direccion direccion) {
+        DatosDireccion datosDireccion) {
 }
