2621d5d70f2fd446b31dd06e447632dfc28b4056
Miguel Ángel de la Cruz Lázaro <256243310+Miguel-Dark@users.noreply.github.com>
2026-02-24 09:42:55 -0600

    feat: implement ValidadorPacienteSinOtraConsultaEnElMismoDia and existsByPacienteIdAndFechaBetween in ConsultaRepository

--- src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
+++ src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
@@ -1,6 +1,10 @@
 package med.voll.api.domain.consulta;
 
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+    boolean existsByPacienteIdAndFechaBetween(@NotNull Long idPaciente, LocalDateTime primerHorario, LocalDateTime ultimoHorario);
 }
