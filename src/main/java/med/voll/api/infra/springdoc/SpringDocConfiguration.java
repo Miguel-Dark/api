95aba1e77a905b088983fcdc303b482b3ceff1ed
Miguel Ángel de la Cruz Lázaro <256243310+Miguel-Dark@users.noreply.github.com>
2026-03-05 20:54:23 -0600

    feat: simplify swagger security with global requirement

--- src/main/java/med/voll/api/infra/springdoc/SpringDocConfiguration.java
+++ src/main/java/med/voll/api/infra/springdoc/SpringDocConfiguration.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,6 +28,7 @@ public class SpringDocConfiguration {
                                 .email("apache-dark@gmail.com"))
                         .license(new License()
                                 .name("Apache 2.0")
-                                .url("http://voll.med/api/licencia")));
+                                .url("http://voll.med/api/licencia")))
+                .addSecurityItem(new SecurityRequirement().addList("bearer-key"));
     }
 }
